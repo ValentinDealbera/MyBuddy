79273104b731d706ee52bb5d140fcb7122557cff
Valentin Dealbera <valentindealbera01@gmail.com>
2023-03-12 16:20:31 -0300

    4/5 routes complete

--- api/src/models/Temperament.js
+++ api/src/models/Temperament.js
@@ -7,12 +7,15 @@ module.exports = (sequelize) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
+      validate: true,
+      primaryKey: true,
     },
     id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-      allowNull: false
     },
+  },{
+    timestamps: false
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const axios = require('axios')
-const {Dog} = require('../db')
+const {Dog, Temperament, conn} = require('../db')
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -18,6 +18,20 @@ router.get('/dogs', async (req, res)=>{
     }
 })
 
+router.get('/dogs/name', async (req, res)=>{
+    try {
+        const {name} = req.query
+        const dogsApi = await axios.get('https://api.thedogapi.com/v1/breeds')
+        const dataBaseDogs = await Dog.findAll()
+        const allDogs = [...dogsApi.data, ...dataBaseDogs]
+        const specificDog = allDogs.filter(e=>e.name.toLowerCase().includes(name))
+        console.log(name);
+        res.status(200).json(specificDog)
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+})
+
 router.get('/dogs/:idRaza', async (req, res)=>{
     try {
         const {idRaza} = req.params
@@ -25,11 +39,39 @@ router.get('/dogs/:idRaza', async (req, res)=>{
         const dataBaseDogs = await Dog.findAll()
         const allDogs = [...dogsApi.data, ...dataBaseDogs]
         const specificDog = allDogs.filter(e=>e.id === Number(idRaza))
+        console.log(idRaza);
         res.status(200).json(specificDog)
     } catch (error) {
         res.status(500).json({error: error.message})
     }
 })
 
+router.get('/temperaments', async (req, res)=>{
+    try {
+        const dogsApi = await axios.get('https://api.thedogapi.com/v1/breeds')
+        let arr = []
+        const aux = dogsApi.data.map(e=>{
+            return e.temperament + ''
+        }).map(e=>{
+            return e.split(',')
+        })
+        for (let i = 0; i < aux.length; i++) {
+            for (let x = 0; x < aux[i].length; x++) {
+                aux[i][x]
+                arr.push(aux[i][x])
+            }
+        }
+        const set = new Set(arr)
+        const finalArr = Array.from(set).map(e=>e.trim()).map(element=>{
+            return {
+                name: element
+            }
+        })
+        await Temperament.bulkCreate(finalArr, {ignoreDuplicates: true})
+        res.status(200).send('Temperaments added succesfully!')
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+})
 
 module.exports = router;

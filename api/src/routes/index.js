578b1624fa3d2c31eb64e1fdd2cc99ceb77d2ac6
Valentin Dealbera <valentindealbera01@gmail.com>
2023-03-12 18:35:55 -0300

    Back-end finished(?

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -11,8 +11,35 @@ const router = Router();
 router.get('/dogs', async (req, res)=>{
     try {
         const dogsApi = await axios.get('https://api.thedogapi.com/v1/breeds')
-        const dataBaseDogs = await Dog.findAll()
-        res.status(200).json([...dogsApi.data, ...dataBaseDogs])
+        const dataBaseDogs = await Dog.findAll({include: {
+            model: Temperament,
+            attributes: ['name'],
+            through: {
+                attributes: []
+            }
+        }})
+        const formatedDatabaseDogs = dataBaseDogs.map(e=>{
+            return {
+            id: e.id,    
+            name: e.name,
+            image: e.image,
+            height: e.height,
+            weight: e.weight,
+            life_span: e.life_span,
+            temperament: dataBaseDogs[0].temperaments.map(e=>e.dataValues.name).join(', ')
+        }})
+        const formatedDogs = dogsApi.data.map(e=>{
+            return {
+                name: e.name,
+                id: e.id,
+                image: e.image.url,
+                weight: e.weight.metric,
+                height: e.height.metric,
+                life_span: e.life_span,
+                temperament: e.temperament
+            }
+        })
+        res.status(200).json([...formatedDogs, ...formatedDatabaseDogs])
     } catch (error) {
         res.status(500).json({error: error.message})
     }
@@ -22,8 +49,33 @@ router.get('/dogs/name', async (req, res)=>{
     try {
         const {name} = req.query
         const dogsApi = await axios.get('https://api.thedogapi.com/v1/breeds')
-        const dataBaseDogs = await Dog.findAll()
-        const allDogs = [...dogsApi.data, ...dataBaseDogs]
+        const dataBaseDogs = await Dog.findAll({include: {
+            model: Temperament,
+            through: {
+                attributes: []
+            }
+        }})
+        const formatedDatabaseDogs = dataBaseDogs.map(e=>{
+            return {
+            id: e.id,
+            name: e.name,
+            image: e.image,
+            height: e.height,
+            weight: e.weight,
+            life_span: e.life_span,
+            temperament: dataBaseDogs[0].temperaments.map(e=>e.dataValues.name).join(', ')
+        }})
+        const formatedDogs = dogsApi.data.map(e=>{
+            return {
+                name: e.name,
+                id: e.id,
+                image: e.image.url,
+                weight: e.weight.metric,
+                height: e.height.metric,
+                life_span: e.life_span
+            }
+        })
+        const allDogs = [...formatedDogs, ...formatedDatabaseDogs]
         const specificDog = allDogs.filter(e=>e.name.toLowerCase().includes(name))
         if (!name){
             return res.status(400).json({error: 'Ingresa una raza'})
@@ -41,10 +93,35 @@ router.get('/dogs/:idRaza', async (req, res)=>{
     try {
         const {idRaza} = req.params
         const dogsApi = await axios.get('https://api.thedogapi.com/v1/breeds')
-        const dataBaseDogs = await Dog.findAll()
-        const allDogs = [...dogsApi.data, ...dataBaseDogs]
-        const specificDog = allDogs.filter(e=>e.id === Number(idRaza))
-        console.log(idRaza);
+        const dataBaseDogs = await Dog.findAll({include: {
+            model: Temperament,
+            through: {
+                attributes: []
+            }
+        }})
+        const formatedDatabaseDogs = dataBaseDogs.map(e=>{
+            return {
+            id: e.id,
+            name: e.name,
+            image: e.image,
+            height: e.height,
+            weight: e.weight,
+            life_span: e.life_span,
+            temperament: dataBaseDogs[0].temperaments.map(e=>e.dataValues.name).join(', ')
+        }})
+        const formatedDogs = dogsApi.data.map(e=>{
+            return {
+                name: e.name,
+                id: e.id,
+                image: e.image.url,
+                weight: e.weight.metric,
+                height: e.height.metric,
+                life_span: e.life_span
+            }
+        })
+        const allDogs = [...formatedDogs, ...formatedDatabaseDogs]
+        const specificDog = idRaza.length > 10 ? allDogs.filter(e=>e.id === idRaza) : allDogs.filter(e=>e.id === Number(idRaza))
+        if(idRaza){}
         res.status(200).json(specificDog)
     } catch (error) {
         res.status(500).json({error: error.message})
@@ -84,11 +161,11 @@ router.post('/dogs', async (req, res)=>{
         const dataBaseDogs = await Dog.findAll()
         const allDogs = [...dogsApi.data, ...dataBaseDogs]
     try {
-        const {name, image, height, weight, life_span} = req.body
+        const {name, image, height, weight, life_span, temperament} = req.body
         if (allDogs.find(e=>e.name.toLowerCase() === name.toLowerCase())){
             return res.status(400).json({error: 'La raza ingresada ya existe'})
         }
-        if (!name || !image || !height || !weight || !life_span){
+        if (!name || !image || !height || !weight || !life_span || !temperament){
             return res.status(400).json({error: 'Debes completar todos los datos'})
         }
         const dog = {
@@ -98,8 +175,27 @@ router.post('/dogs', async (req, res)=>{
             weight,
             life_span
         }
-        await Dog.create(dog)
-        return res.status(201).json(dog) 
+        const newDog = await Dog.create(dog)
+        await newDog.addTemperaments(temperament)
+        const aux = await Dog.findAll({where:{
+            name: name
+        },include: {
+            model: Temperament,
+            attributes: ['name'],
+            through: {
+                attributes: []
+            }
+        }})
+        console.log(aux[0].temperaments[0].dataValues.name);
+        const showDog = {
+            name,
+            image,
+            height,
+            weight,
+            life_span,
+            temperament: aux[0].temperaments.map(e=>e.dataValues.name).join(', ')
+        }
+        return res.status(201).json(showDog) 
     } catch (error) {
       res.status(500).json({error: error.message})  
     }

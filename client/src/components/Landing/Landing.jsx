03b6928024e849400f84cf858eb496b66a85794c
Valentin Dealbera <valentindealbera01@gmail.com>
2023-03-20 14:22:06 -0300

    landing done

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,12 +1,21 @@
 import { Link } from "react-router-dom"
+import styles from "./Landing.module.css"
 
 const Landing = () => {
     return (
-        <div>
-            <h1>esta es una landing page</h1>
+        <div className={styles.back}>
+            <div className={styles.image}>
+                <div className={styles.textBox}>
+                    <div className={styles.text}>
+            <h1>Welcome to MyBuddy™</h1>
+            <h3>Looking for a friend?</h3>
+            <h3>Search or create your ideal buddy!</h3>
             <Link to={"/home"}>
-            <button>Home</button>
+            <button className={styles.button}>Go now!</button>
             </Link>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

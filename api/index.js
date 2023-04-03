a7f4ffc5a72bf9e3d5a4361f7cf9a2e29af1914e
Valentin Dealbera <valentindealbera01@gmail.com>
2023-04-03 09:55:09 -0300

    presentation

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || 3001
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('listening at 3001'); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log('listening at ' + port); // eslint-disable-line no-console
   });
 });
